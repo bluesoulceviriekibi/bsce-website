ba573231fa35f2669586eab16d98e599854364f9
omerakgoz34 <omerakgoz34@gmail.com>
2020-04-10 00:14:27 +0300

    some master & a new CDN for anime.js

--- routes/pages.js
+++ routes/pages.js
@@ -4,15 +4,15 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '288', imgHeight: '288', pageURL: '', desc: 'Her şeyi çeviren ekip!' })
+  res.render('index', { title: '', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '100', imgHeight: '100', pageURL: '', desc: 'Her şeyi çeviren ekip!' })
 })
 
 router.get('/ceviriler', function (req, res, next) {
-  res.render('translations', { title: 'Çeviriler | ', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '288', imgHeight: '288', pageURL: '/ceviriler', desc: 'Çevirilerimiz!' })
+  res.render('translations', { title: 'Çeviriler | ', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '100', imgHeight: '100', pageURL: '/ceviriler', desc: 'Çevirilerimiz!' })
 })
 
 router.get('/hakkimizda', function (req, res, next) {
-  res.render('about_us', { title: 'Hakkımızda | ', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '288', imgHeight: '288', pageURL: '/hakkimizda', desc: 'Hakkımızda :3' })
+  res.render('about_us', { title: 'Hakkımızda | ', iconType: 'jpeg', icon: 'bsce.jpg', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'bsce.jpg', imgType: 'jpg', imgWidth: '100', imgHeight: '100', pageURL: '/hakkimizda', desc: 'Hakkımızda :3' })
 })
 
 router.get('/ceviri/deltarune', function (req, res, next) {
@@ -21,6 +21,10 @@ router.get('/ceviri/deltarune', function (req, res, next) {
 
 router.get('/ceviri/escaped_chasm', function (req, res, next) {
   res.render('translations/escaped_chasm', { title: 'Escaped Chasm Türkçe | ', iconType: 'png', icon: 'escaped_chasm_icon.png', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)'", img: 'escaped_chasm.png', imgType: 'png', imgWidth: '633', imgHeight: '484', pageURL: '/ceviri/escaped_chasm', desc: 'Deltarune Türkçe - v1 - 03.06.2019' })
+})
+
+router.get('/ceviri/escaped-chasm', function (req, res, next) {
+  res.render('redirect', { url: '/ceviri/escaped_chasm' })
 })
 
 router.get('/ceviri/noragami_aragoto_op', function (req, res, next) {
@@ -33,6 +37,10 @@ router.get('/ceviri/steins_gate_0_last_game', function (req, res, next) {
 
 router.get('/poke_preview', function (req, res, next) {
   res.render('poke_preview')
+})
+
+router.get('/ceviri/the_aftermath', function (req, res, next) {
+  res.render('translations/the_aftermath', { title: 'The Aftermath Türkçe | ', iconType: 'icon', icon: 'the_aftermath.ico', themeColor: '#0F3280', bodyAttr: " onload='startStars(1000)' onkeydown='ControlKey(event)'", img: 'the_aftermath_logo.png', imgType: 'png', imgWidth: '326', imgHeight: '203', pageURL: '/ceviri/the_aftermath', desc: 'The Aftermath Türkçe - v1.0 - 09.04.2020' })
 })
 
 module.exports = router
