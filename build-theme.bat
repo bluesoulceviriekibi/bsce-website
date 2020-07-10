13ed9b93dbb3720d3a668a95941cc25888d791f9
omerakgoz34 <omerakgoz34@gmail.com>
2020-07-10 16:33:55 +0300

    Disqus renk sorunu düzeltildi

--- build-theme.bat
+++ build-theme.bat
@@ -3,4 +3,4 @@ del bilberry-hugo-theme\static\theme.css
 del static\theme.css
 mkdir bilberry-hugo-theme\assets\sass
 copy /Y assets\sass\* bilberry-hugo-theme\assets\sass
-cd bilberry-hugo-theme && node_modules\.bin\cross-env.cmd NODE_ENV=production node_modules/webpack/bin/webpack.js --no-progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js && cd .. && copy /Y bilberry-hugo-theme\static\theme.css static\theme.css
+cd bilberry-hugo-theme && node_modules\.bin\cross-env.cmd NODE_ENV=production node_modules/webpack/bin/webpack.js --no-progress --hide-modules --config=node_modules/laravel-mix/setup/webpack.config.js && cd .. && copy /Y bilberry-hugo-theme\static\theme.css static\theme.css && pause
