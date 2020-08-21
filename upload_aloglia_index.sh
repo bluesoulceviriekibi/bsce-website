9a15bb7ed4ed15b60ec07c952e57a549323ea75c
omerakgoz34 <49201485+omerakgoz34@users.noreply.github.com>
2020-08-21 14:32:17 +0300

    Update upload_aloglia_index.sh

--- upload_aloglia_index.sh
+++ upload_aloglia_index.sh
@@ -5,7 +5,7 @@ app_id="$ALGOLIA_APP_ID"
 admin_api_key="$ALGOLIA_ADMIN_API_KEY"
 index_name="bluesoulceviriekibi"
 
-python algolia/index-upload.py \
+python upload_aloglia_index.py \
     -f "$index_file" \
     -a "$app_id" \
     -k "$admin_api_key" \
